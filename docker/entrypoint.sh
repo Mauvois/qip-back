7b8eabf7a168c4d2874aa6f9f5eac64267d23e41
Mauvois <roger.mauvois@gmail.com>
2025-04-24 07:58:30 +0200

    fix entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 set -e
 
-# Si la variable base64 est fournie, on la décode dans /secrets
-if [ -n "$GOOGLE_CREDENTIALS_B64" ]; then
+# Crée le dossier s’il n’existe pas
+mkdir -p /secrets
+
+# Si la variable est présente, décode et écrit le fichier
+if [ -n "$GOOGLE_CREDENTIALS_B64" ] ; then
   echo "$GOOGLE_CREDENTIALS_B64" | base64 -d > /secrets/credentials.json
   export GOOGLE_APPLICATION_CREDENTIALS=/secrets/credentials.json
 fi
